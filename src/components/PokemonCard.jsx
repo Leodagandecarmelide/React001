b4638489b585be593fec37d7b2b08cfbb45c5243
Leodagandecarmelide <leodaganditlesanguinaire@gmail.com>
2024-06-11 23:16:57 +0200

    Fin quête 02

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+  return (
+    <div>
+      Je suis PokemonCard
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Elephant at sunset"
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </div>
+  );
+}
+export default PokemonCard;
